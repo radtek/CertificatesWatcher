b3f1b36db0df8393e5585ee5bdc0b71eec24367a
Yuriy Nagaev <me@ynagaev.ru>
2013-07-19 22:16:14 +0400

    Название организации из субъекта сертификата
    В рассылке теперь указывается название орагнизации (параметр 'O') из
    свойства Subject сертификата.

---
+++ CertficatesWatcher/CertHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace CertficatesWatcher
+{
+    public static class CertHelper
+    {
+        private static readonly Regex OrganizationRegex = new Regex(@"O *= *(.+?)($|,)", RegexOptions.Multiline);
+
+        public static string GetOrganization(this X509Certificate cert)
+        {
+            var organizationMatch = OrganizationRegex.Match(cert.Subject);
+
+            if (organizationMatch.Success)
+            {
+                return organizationMatch.Groups[1].Value;
+            }
+
+            return "unknown";
+        }
+    }
+}
